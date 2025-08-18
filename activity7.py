1bdf52e88c95c7ffff3e3cd4a3d6b0e31d8d2756
Daniel Oco <ocodaniel13@gmail.com>
2025-08-18 15:15:57 +0800

    first commit

---
+++ activity7.py
@@ -0,0 +1,19 @@
+a = 5
+
+print("the value of a is", a)
+
+a += 5
+
+print("the value of a is", a)
+
+a += 13
+
+print("the value of a is", a)
+
+a += 20
+a -= 5
+a *= 3
+a += 8
+a - a + 10
+
+print("the value of a is", a)
