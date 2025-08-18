0a87ed6232ca2bd686644dd71575069071cb262b
Daniel Oco <ocodaniel13@gmail.com>
2025-08-18 15:45:51 +0800

    first commit

---
+++ activity9.py
@@ -0,0 +1,12 @@
+a = 20
+b = 15
+
+c = 93
+d = 70
+
+username = 'daniel'
+password = 'ekawlangsapatna143'
+print((username == 'daniel') and (password == 'ekawlangsapatna143'))
+#print( (a > b) and ( c < d) )
+#print( (a > b) or (c < d) )
+#print(not(a > b) or (c < d) )
