3ce1a94bfbe559906a432f924c9d75e9e31822d6
Daniel Oco <ocodaniel13@gmail.com>
2025-09-15 15:56:11 +0800

    Delete activity13.py

--- activity13.py
+++
@@ -1,9 +0,0 @@
-#ask user to input 10(ten) numbers 
-#after get the summation of all numbers
-
-sum = 0
-for d in range(1, 6, 1,):
-	print(d)
-	number = eval(input("Enter anything ---> "))
-	sum += number
-print("The sum of all the numbers ",sum)
