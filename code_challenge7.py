d52daaf65260c9d59a96915f2c19529126eadc48
Daniel Oco <ocodaniel13@gmail.com>
2025-09-17 12:46:26 +0800

    Create code_challenge7.py

---
+++ code_challenge7.py
@@ -0,0 +1,13 @@
+# code_challenge7.py
+# Countdown Timer Simulator
+
+print("COUNTDOWN TIMER SIMULATOR")
+
+start_num = int(input("Enter the starting number for countdown: "))
+
+print("\nCountdown started:")
+
+for i in range(start_num, 0, -1):
+    print(i)
+
+print("Liftoff!")
