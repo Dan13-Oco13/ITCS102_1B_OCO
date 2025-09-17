84d2fc208b8592af563ffd1a2221c5555f2bd764
Daniel Oco <ocodaniel13@gmail.com>
2025-09-17 12:58:57 +0800

    Create code_challenge8.py

---
+++ code_challenge8.py
@@ -0,0 +1,8 @@
+print("MULTIPLICATION TABLE MAKER")
+
+number = int(input("Enter a number: "))
+
+print(f"\nMultiplication table for {number}:")
+
+for i in range(1, 11):
+    print(f"{number} x {i} = {number * i}")
