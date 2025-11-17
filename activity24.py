256544f26c7347077187de116f3c47aec9095035
Daniel Oco <ocodaniel13@gmail.com>
2025-11-17 14:42:29 +0800

    Create activity24.py

---
+++ activity24.py
@@ -0,0 +1,12 @@
+from activity24_1 import another, NLA, Return, Factorial
+
+def Greetings():
+    print("Hi! This is a Function\nWhat it does is Greet People\nHi VISITOR")
+    
+Greetings()
+another('Dan’)
+NLA('Dan','Lucena','18')
+
+print(f"i want to get the summation of 1 to {Return(8)}")
+print(f"i want to get the summation of 1 to {Return(8)+10}")
+print(f"The factorial of 3 is {Factorial(3)}")
